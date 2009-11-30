10671defee7c8f5314f4d1ab99351653d224b7e9
Priya Komarlingam <knspriya@quicinc.com>
2009-11-30 13:57:34 -0800

    Disabling libcamera2 and including the right Android.mk for pvomx in order to resolve compilation errors.

--- pvomx/Android.mk
+++ pvomx/Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 
 $(call add-prebuilt-files, ETC, 01_qcomm_omx.cfg)
 
-include vendor/qcom/android/pvomx/omx_core_plugin/Android.mk
+include vendor/qcom/android-open/pvomx/omx_core_plugin/Android.mk
 endif
